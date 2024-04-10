38a1e2fb5cabb22e99ab24519b14207453c96907
Jealous Matsikachando <pasej5@gmail.com>
2024-04-10 04:51:30 +0200

    update room

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name': 'lets learn python'},
-    {'id':2, 'name': 'Design with me'},
-    {'id':3, 'name': 'Front end developers'},
-]
+# rooms = [
+#     {'id':1, 'name': 'lets learn python'},
+#     {'id':2, 'name': 'Design with me'},
+#     {'id':3, 'name': 'Front end developers'},
+# ]
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    
