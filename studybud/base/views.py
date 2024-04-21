c6a7375c9003ced7f623576c49790d7f78935443
Jealous Matsikachando <pasej5@gmail.com>
2024-04-21 20:30:54 +0200

    adding participants

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -24,17 +24,20 @@ def loginPage(request):
     
     if request.method == "POST":
         username = request.POST.get('username').lower()
+
         password = request.POST.get('password')
         
         try:
             user = User.objects.get(username=username)
         except:
             messages.error(request, "User does not exists.")
-        return redirect('login')
+            return redirect('login')
         
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
+
             return redirect('home')
         else:
             messages.error(request, "Username or Password does not exist.")
@@ -81,17 +84,18 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-    
+    participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     
     
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
